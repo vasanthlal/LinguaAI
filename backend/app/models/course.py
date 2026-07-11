f5d6730692a4a3349fb0957e6bdab5eb71dd8bd0
vasanthlal <vasanthlal7181@gmail.com>
2026-07-11 21:38:09 +0530

    feat: complete Course module

---
+++ backend/app/models/course.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from sqlalchemy.orm import relationship
+
+from app.database.base import Base
+
+
+class Course(Base):
+    __tablename__ = "courses"
+
+    id = Column(Integer, primary_key=True, index=True)
+    language_id = Column(Integer, ForeignKey("languages.id"), nullable=False)
+
+    title = Column(String(150), nullable=False)
+    description = Column(Text, nullable=True)
+    level = Column(String(50), nullable=False)
+
+    language = relationship("Language", backref="courses")
